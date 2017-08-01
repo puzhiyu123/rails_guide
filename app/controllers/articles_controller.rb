ac769e0b28ff03bd453478741c448053bdd144bd
Zhiyu Pu <georgepu45@gmail.com>
2017-08-01 19:12:48 -0400

    create Article controller, model, routes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,28 @@
 class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all ## @articles and NOT @article
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+
+  def create
+    @article =
+        Article.new(article_params)
+    @article.save
+    redirect_to @article
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+
 end
